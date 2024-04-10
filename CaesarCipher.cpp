db61acfe8e1bbd7a2fbb8db042ae098f82b961b8
xawellington <158227468+xawellington@users.noreply.github.com>
2024-04-10 18:56:50 -0400

    Update CaesarCipher.cpp

--- CaesarCipher.cpp
+++ CaesarCipher.cpp
@@ -5,11 +5,46 @@ const string CaesarCipher::CIPHER_NAME = "Caesar";
 CaesarCipher::CaesarCipher(int shift) : Cipher(CIPHER_NAME), shift(shift){}
 
 bool CaesarCipher::encode(string decodedMsg) {
-  /*TODO: write your code*/
+  string encodedMsg = "";
+    for (char c : decodedMsg) {
+        if (c == ' ') {
+            encodedMsg += ' ';
+        } else if (isValidChar(c)) {
+            int charNum = c - 'A';
+            charNum -= shift;
+            if (charNum > 25)
+                charNum -= 26;
+            else if (charNum < 0)
+                charNum += 26;
+            char encodedChar = charNum + 'A';
+            encodedMsg += encodedChar;
+        } else {
+            return "False";
+        }
+    }
+    return encodedMsg;
 }
 
 bool CaesarCipher::decode(string encodedMsg) {
-  /*TODO: write your code*/
+  string decodedMsg = "";
+    for (char c : encodedMsg) {
+        if (c == ' ') {
+            decodedMsg += ' ';
+        } else if (isValidChar(c)) {
+            int charNum = c - 'A'; 
+            charNum += shift;
+            if (charNum > 25)
+                charNum -= 26;
+            else if (charNum < 0)
+                charNum += 26;
+            char decodedChar = charNum + 'A'; 
+            decodedMsg += decodedChar;
+        } else {
+            return "False";
+        }
+    }
+    return decodedMsg;
+}
 }
 
 bool CaesarCipher::isValidCharacter(char c) {
